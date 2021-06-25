52f67a16cc768325a1ceb3d98ae20f181537b0dc
Michael Wilson <michael.wilson@nelnet.net>
2021-06-25 08:52:09 -0500

    Vehicle changes

---
+++ Vehicle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+       internal  class vehicle
+        {
+
+            public int numWheels;
+
+            Car myCar = new Car();
+
+        
+            protected class Car : vehicle
+            {
+            
+            
+            }
+
+            protected class Semi : vehicle
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
